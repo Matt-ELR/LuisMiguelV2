de6966101be0903fd74c8def8856719586cf3866
Matt-ELR <mattiasemile@gmail.com>
2024-11-12 12:07:52 -0600

    Login Basico funcionando

---
+++ Paciente/panel-paciente.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if ($_SESSION['account_type'] !== 'admin') {
+    header("Location: ../Sesion/login.html"); // Redirect if not an admin
+    exit();
+}
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+</head>
+<body>
+
+    <h1>Admin Dashboard</h1>
+    <p>Welcome, <?php echo $_SESSION['username']; ?>! This page is for paciente only.</p>
+    
+</body>
+</html>
